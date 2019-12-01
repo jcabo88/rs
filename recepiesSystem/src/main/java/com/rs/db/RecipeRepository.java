fe297fce4141df7d354e289f9e9f910fd837eab5
jcabo <57321623+jcabo88@users.noreply.github.com>
2019-12-01 13:43:13 +0100

    Add mssql connection to the system
    - fetch dependencies
    - DTO example for a table
    - Create interface for JPA Repository

---
+++ recepiesSystem/src/main/java/com/rs/db/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.rs.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository {
+
+}
