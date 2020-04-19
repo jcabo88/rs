44e85a9c492e767a4fd2eaac229fa5dc6969d01e
Jesus Cabo <jesus.caboc@gmail.com>
2020-04-19 13:15:49 +0200

    Define System output
    Rework the output to return a message or a recipe. Turn off the DB dependencies.

--- recepiesSystem/src/main/java/com/rs/controller/ProcessResult.java
+++ recepiesSystem/src/main/java/com/rs/controller/ProcessResult.java
@@ -1,5 +1,6 @@
 package com.rs.controller;
 
+import com.rs.controller.dto.RecipeResult;
 import com.rs.controller.dto.Result;
 import com.rs.db.RecipesMockedData;
 import com.rs.db.RecipesStorage;
@@ -9,22 +10,32 @@ import java.util.Optional;
 
 public class ProcessResult {
 
-    private Result result;
+    private static final String RECIPE_NOT_FOUND = "Recipe not Found";
     private final RecipesStorage recipesStorage = RecipesMockedData.getInstance();
 
-    //TODO Return an object Recipe inside the object Result
     public Optional<Result> getRecipesByID(int id){
-        recipesStorage.getRecipeByIndex(id)
-                .ifPresent(this::getRecipe);
-        // TODO When a result does not exist, the previous result is found
-        return Optional.of(this.result);
+        return getRecipeResultByID(id)
+                .flatMap(this::getResult);
     }
 //    TODO Return List of Recipes
 //    public Optional<Recipe> getRecipesByName(String name){
 //        return recipesStorage.getRecipeByName(name);
 //    }
 
-    private void getRecipe(Recipe recipe) {
-        this.result = new Result(recipe);
+    private Optional<RecipeResult> getRecipeResultByID(int id){
+        return recipesStorage.getRecipeByIndex(id)
+                .map(this::getRecipeResult)
+                .orElseGet(() -> Optional.ofNullable(
+                        new RecipeResult.Builder().message(RECIPE_NOT_FOUND).build()));
+    }
+
+    private Optional<RecipeResult> getRecipeResult(Recipe recipe){
+        return Optional.ofNullable(new RecipeResult.Builder()
+                .recipe(recipe)
+                .build());
+    }
+
+    private Optional<Result> getResult(RecipeResult recipe) {
+        return Optional.of(new Result(recipe));
     }
 }
